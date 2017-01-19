6162764e2403f581288dfe5c061c8cf9f51c046a
atharv bhute <atharv.bhute18@gmail.com>
2017-01-20 01:40:01 +0530

    upload e done

--- database/migrations/2017_01_19_173253_create_events_table.php
+++ database/migrations/2017_01_19_173253_create_events_table.php
@@ -15,7 +15,7 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->nullable();
+            $table->integer('user_id');
             $table->text('name');
             $table->text('organiser');
             $table->text('address');
