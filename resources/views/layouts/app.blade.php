3bd4f57539f44a60e0c4c72d57bb98b7ac88674a
atharv bhute <atharv.bhute18@gmail.com>
2017-01-24 13:33:29 +0530

    working bit well but still its without admin verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,25 +8,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>thecompete</title>
+    <title><strong>thecompete</strong>(<small>prototype</small>)</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script
-            src="https://code.jquery.com/jquery-3.1.1.min.js"
-            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    @yield('script')
 
     <!-- Scripts -->
     <script>
@@ -34,7 +19,6 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
-
 </head>
 <body>
     <div id="app">
@@ -64,27 +48,30 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
+
                         <li><a href="{{ url('/upload') }}">Upload Event</a></li>
 
+                        <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
-                            <li>
-                                <a href="{{ route('dash',['id'=>Auth::user()->id]) }}">
-                                    Dashboard
-                                </a>
-                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+
+                                    <li>
+                                        <a href="{{ route('dash',['id'=>Auth::user()->id]) }}">
+                                            Dashboard
+                                        </a>
+                                    </li>
+
                                     <li>
                                         <a href="{{ url('/logout') }}"
-                                           onclick="event.preventDefault();
+                                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
@@ -95,7 +82,6 @@
                                     </li>
                                 </ul>
                             </li>
-
                         @endif
                     </ul>
                 </div>
