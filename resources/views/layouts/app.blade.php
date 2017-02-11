90133c8ae6227f777942eecbffc6f8ea1e59dfeb
atharv bhute <atharv.bhute18@gmail.com>
2017-02-12 00:15:07 +0530

    sidebar categories ready

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,26 +87,6 @@
                 <ul class="nav navbar-nav">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            Categories<span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-
-
-                            @foreach(\App\Category::all() as $category)
-                            <li>
-                                <a href="{{url('/category/'.$category->id)}}">
-                                    {{$category->name}}
-                                </a>
-                            </li>
-                            @endforeach
-
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             About<span class="caret"></span>
                         </a>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,31 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="row">
-{{--<section class="events endless-pagination" data-next-page="{{ $events->nextPageUrl() }}">--}}
+            <div class="col-md-12">
+            <div class="col-md-3">
+                <p class="lead hidden-sm hidden-xs">Categories</p>
+                    <div class="form-group hidden-md hidden-lg">
+                        <div >
+                            <select name="" id="" class="form-control" onchange="location = this.value;">
+                                @foreach(\App\Category::all() as $category)
+                                    <option value="{{url("/category/$category->id")}}">
+                                        {{$category->name}}
+                                    </option>
+                                @endforeach
+
+                            </select>
+                        </div>
+                    </div>
+
+
+                <div class="list-group hidden-sm hidden-xs">
+                    @foreach(\App\Category::all() as $category)
+                            <a href="{{url("/category/$category->id")}}" class="list-group-item">{{$category->name}}</a>
+                    @endforeach
+                </div>
+            </div>
+            <div class="col-md-8">
+
     @foreach($events as $event)
         <a href="{{route('event',['id'=>$event->id])}}">
             <div class="col-sm-6 col-xs-12 col-md-4">
@@ -18,7 +40,7 @@
                         <p><Strong>Name: </Strong>{{$event->name}}</p>
                         <p><Strong>Organiser: </Strong>{{$event->organiser}}</p>
                         <p><Strong>Date: </Strong>{{$event->date}}</p>
-                        <p><Strong>Address: </Strong>{{$event->address}}</p>
+                        {{--<p><Strong>Address: </Strong>{{$event->address}}</p>--}}
                     </div>
                 </div>
             </div>
@@ -28,7 +50,7 @@
    <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12">
        <div class="col-md-offset-4 col-sm-offset-4 col-xs-offset-4 col-lg-offset-4 bottom">{{$events->links()}}</div>
    </div>
-{{--</section>--}}
+
 
 
                     {{--<script>--}}
