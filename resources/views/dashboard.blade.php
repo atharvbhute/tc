219baa8e37686a73182e6c599249ade2130a50ec
atharv bhute <atharv.bhute18@gmail.com>
2017-03-02 00:35:33 +0530

    col-md-offset-2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-8 col-md-offset-2">
                 <div class="row">
                     @include('partials.flash')
                     @if(count($events) == 0)
